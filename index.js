75d60a342a6d58fadcad2839484c56e64c7cb3e7
manasbarikuidev85 <118952876+manasbarikuidev85@users.noreply.github.com>
2024-05-26 14:24:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -58,6 +58,15 @@ for(let i=num.length-1; i>=0; i--){
 }
 console.log(reverseString);
 
+//Q-How to sum an Array ?
+let araayNumber =[10,20,30,40,50];
+function sum(arr){
+return arr.reduce((acc, crrV)=>{
+    return acc +crrV;
+},0)
+}
+let result =sum(araayNumber);
+console.log(result);
 
 
 
