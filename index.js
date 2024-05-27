7ef024947f787c01f4474a17853f44e9bf456f92
manasbarikuidev85 <118952876+manasbarikuidev85@users.noreply.github.com>
2024-05-27 16:03:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -209,6 +209,18 @@ return arr.flat(Infinity);
 }
 console.log(flattenNestedArray(flattenArray));
 
+//Q-write a function to find out first non-repeating char in a string ?
+let string ="bangalore"
+function firstNonRepeteingChar(str){
+for(let i=0; i<str.length; i++){
+if(str.indexOf(str[i])=== str.lastIndexOf(str[i])){
+return str[i]
+}
+}
+return null;
+}
+console.log(firstNonRepeteingChar(string));
+
 
 
 
