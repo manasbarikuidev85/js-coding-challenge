def48f8088298237cdef1bfd04a20813ec470e23
manasbarikuidev85 <118952876+manasbarikuidev85@users.noreply.github.com>
2024-05-27 15:28:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -163,7 +163,24 @@ newArray.map((item,i)=>{
 })
 console.log(array.toString().replaceAll(',',' '));
 
+//Q-Write a function that prints numbers from 1 to 100. For multiples of 3, 
+//print "Fizz" instead of the number, and for multiples of 5, print "Buzz". 
+//For numbers which are multiples of both 3 and 5, print "FizzBuzz"?
 
+function fizzBuzz(){
+    for(let i=1; i<=100; i++){
+        if(i % 3 === 0 && i % 5 === 0){
+            console.log("fizzBuzz")
+        }else if(i % 3 === 0){
+            console.log("fizz")
+        }else if(i % 5 === 0){
+            console.log("Buzz")
+        }else{
+            console.log(i)
+        }
+    }
+}
+fizzBuzz()
 
 
 
